40bec9a04293cf2d35a8dd99362b17d622b66147
willam <william@carrot-i.com>
2024-05-14 13:23:31 +0900

    test - 데이터 렌더링 및 삭제 버튼

--- src/features/Test.tsx
+++ src/features/Test.tsx
@@ -1,8 +1,9 @@
 'use client';
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { FieldValues, useForm } from 'react-hook-form';
 export default function Test() {
   const { register, handleSubmit } = useForm({ mode: 'onBlur' });
+  const [contentData, setContentData] = useState<{ id: string; content: string; title: string }[] | null>(null);
   const getData = async () => {
     const response = await fetch('/api/test', {
       method: 'GET',
@@ -22,8 +23,8 @@ export default function Test() {
       body: JSON.stringify(value)
     });
   };
-  const deleteData = async () => {
-    const response = await fetch('/api/test/6642d80593b67a604a078681', {
+  const deleteData = async (id: string) => {
+    const response = await fetch(`/api/test/${id}`, {
       method: 'DELETE',
       headers: {
         'Content-Type': 'application/json'
@@ -35,10 +36,16 @@ export default function Test() {
   }, []);
   return (
     <div>
+      {contentData?.map((data) => (
+        <>
+          <p>{data.title}</p>
+          <p>{data.content}</p>
+          <button onClick={() => deleteData(data.id)}>삭제</button>
+        </>
+      ))}
       <input className="text-black" type="text" {...register('title')} />
       <input className="text-black" type="text" {...register('content')} />
       <button onClick={handleSubmit((value) => setData(value))}>저장</button>
-      <button onClick={deleteData}>삭제</button>
     </div>
   );
 }
