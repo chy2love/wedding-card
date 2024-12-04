3366112ae18a7bd6f68420b814f486873d067cab
최하영 <chlgkduddlek@gmail.com>
2024-12-04 12:23:42 +0900

    #4

--- src/features/components/home/howToCome/Map.tsx
+++ src/features/components/home/howToCome/Map.tsx
@@ -7,13 +7,11 @@ declare global {
     kakao: any;
   }
 }
-const KAKAO_SDK_URL = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_MAP_API_KEY}&autoload=false`;
 export function Map() {
   useEffect(() => {
     const kakao = window.kakao;
     kakao.maps.load(() => {
       // 2. 지도 생성 및 설정
-      console.log(' => ', KAKAO_SDK_URL);
       const container = document.getElementById('map');
       const options = {
         center: new kakao.maps.LatLng(37.449904, 127.12713),
@@ -30,7 +28,6 @@ export function Map() {
 
   return (
     <>
-      <Script src={KAKAO_SDK_URL} strategy="beforeInteractive" />
       <div className="w-full h-[250px]" id="map"></div>
     </>
   );
