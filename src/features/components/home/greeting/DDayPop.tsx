b69298f0020fc0804ad937a3e3cad082f7507a01
최하영 <chlgkduddlek@gmail.com>
2024-12-18 15:23:11 +0900

    #디데이 팝업 위로 올림

--- src/features/components/home/greeting/DDayPop.tsx
+++ src/features/components/home/greeting/DDayPop.tsx
@@ -5,7 +5,7 @@ export function DDayPop() {
   const diff = Math.ceil(moment.duration(weddingDay.diff(today)).asDays());
 
   return (
-    <div className="absolute w-[353px] h-[49px] bg-[#FF1D89] flex items-center px-5 rounded-[6px] top-[233px] left-[50%] translate-x-[-50%]">
+    <div className="absolute w-[353px] h-[49px] bg-[#FF1D89] flex items-center px-5 rounded-[6px] top-[225px] left-[50%] translate-x-[-50%]">
       <p className="text-white text-[11px] border border-solid border-white rounded-[6px] py-1 px-2 mr-2">D-{diff}</p>
       <p className="text-[14px] text-white leading-[19px]">25.01.18 17:30 가천 컨벤션센터 5F</p>
     </div>
