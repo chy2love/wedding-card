df0a1ae528e01a769881b9b5d8e0f4a945216985
최하영 <chlgkduddlek@gmail.com>
2024-12-18 15:18:33 +0900

    #영상 위아래 보더 제거

--- src/features/components/home/greeting/PictureSlider.tsx
+++ src/features/components/home/greeting/PictureSlider.tsx
@@ -3,13 +3,15 @@ import Image from 'next/image';
 
 export function PictureSlider() {
   return (
-    <div className="w-full h-[258px]">
+    <div className="relative w-full h-[258px] ">
       <iframe
-        className="w-full h-full"
+        className="w-full h-full translate-y-[-5px]"
         src="https://www.youtube.com/embed/cktScoOlC-A?si=4f48dGyX5JeQRj-2&amp;controls=0&amp;autoplay=1&amp;mute=1&amp;loop=1&amp;playlist=cktScoOlC-A&amp;modestbranding=0"
         title="YouTube video player"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
       ></iframe>
+      {/* <div className=" absolute top-0 bg-white w-full h-2"></div> */}
+      <div className=" absolute bottom-[5px] bg-white w-full h-[5px]"></div>
     </div>
   );
 }
