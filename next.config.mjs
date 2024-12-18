ea69e60e3aad33455ea36d310d17d9ec1fa5e207
최하영 <chlgkduddlek@gmail.com>
2024-12-18 11:06:19 +0900

    #api prefix 제거

--- next.config.mjs
+++ next.config.mjs
@@ -28,13 +28,13 @@
       }
     ];
   },
-  async rewrites() {
-    return [
-      {
-        source: `/api/:path*`,
-        destination: `${process.env.NEXT_PUBLIC_API_URL}/api/:path*`
-      }
-    ];
-  }
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: `/api/:path*`,
+  //       destination: `${process.env.NEXT_PUBLIC_API_URL}/api/:path*`
+  //     }
+  //   ];
+  // }
 };
 export default nextConfig;
--- src/features/components/home/greeting/PictureSlider.tsx
+++ src/features/components/home/greeting/PictureSlider.tsx
@@ -2,10 +2,5 @@
 import Image from 'next/image';
 
 export function PictureSlider() {
-  const url = 'https://bbosong.co.kr/img/toon/toon1/toon1-1.jpg';
-  return (
-    <div className="w-full h-[258px]">
-      <Image src={url} alt="" className="w-full h-full" width={0} height={0} />
-    </div>
-  );
+  return <div className="w-full h-[258px]"></div>;
 }
