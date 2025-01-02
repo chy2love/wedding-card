d177a72ebd71aa51f68c0259b5199acf7cf752e8
chy2love <chlgkduddlek@gmail.com>
2025-01-03 00:34:44 +0900

    #날짜 팝업 오류 수정

--- src/features/components/home/greeting/DDayPop.tsx
+++ src/features/components/home/greeting/DDayPop.tsx
@@ -1,9 +1,15 @@
+'use client';
+import { useEffect, useState } from 'react';
 import moment from 'moment';
 export function DDayPop() {
-  const today = moment();
-  const weddingDay = moment('2025-01-18');
-  const diff = Math.ceil(moment.duration(weddingDay.diff(today)).asDays());
-
+  const [diff, setDiff] = useState(0);
+  useEffect(() => {
+    const today = moment();
+    const weddingDay = moment('2025-01-18');
+    const tempDiff = Math.ceil(moment.duration(weddingDay.diff(today)).asDays());
+    setDiff(tempDiff);
+    return () => {};
+  }, []);
   return (
     <div className="absolute w-[353px] h-[49px] bg-[#33302F] flex items-center px-5 rounded-[6px] top-[230px] left-[50%] translate-x-[-50%]">
       <p className="text-white text-[11px] border border-solid border-white rounded-[6px] py-1 px-2 mr-2">D-{diff}</p>
